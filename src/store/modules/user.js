d59041031f5638a0cb322e00e810f0486e0633cc
Lotus_fragrance <2369275200@qq.com>
2022-09-01 16:21:07 +0800

    登录完成

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,7 +1,29 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user'
+const state = {
+  token: getToken() || null
+}
+// 修改数据
+const mutations = {
+  setToken(state, token) {
+    state.token = token
+    // 数据存到本地存储
+    setToken(token)
+  },
+  removeToken(state) {
+    state.token = null
+    removeToken()
+  }
+}
+const actions = {
+  async login(context, data) {
+    const result = await login(data)
+    context.commit('setToken', result)
+  } }
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+  actions
 }
 
